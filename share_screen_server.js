bd5a2e17e2006c221bc12afeebf9a2fca859e499
a7261675 <a7261675@gmail.com>
2022-11-21 02:02:02 +0800

    modify findNowRoom function bug

--- share_screen_server.js
+++ share_screen_server.js
@@ -13,8 +13,8 @@ app.get('/', function(req, res){
 const io = require('socket.io')(server);
 
 function findNowRoom(client) {
-  return Object.keys(client.rooms).find(item => {
-      return item !== client.id
+  return Array.from(client.rooms).find(item => {
+      return item.includes('room');
     });
   }
 
@@ -23,26 +23,20 @@ io.on('connection', client => {
   console.log(`socket 用戶連接 ${client.id}`);
 
   client.on('joinRoom', room => {
-    console.log('this is join room');
-    console.log(room);
     const nowRoom = findNowRoom(client);
     if (nowRoom) {
       client.leave(nowRoom);
     }
-    console.log('this is join room2');
-    console.log(client.rooms);
     client.join(room);
-    console.log(client.rooms);
     io.to(room).emit('roomBroadcast', '已有新人加入聊天室！');
   });
 
   client.on('peerconnectSignaling', message => {
     // console.log('接收資料：', message);
-  
     const nowRoom = findNowRoom(client);
-    console.log(client.rooms);
+    console.log('now room: ');
     console.log(nowRoom);
-    client.to('secret room').emit('peerconnectSignaling', message)
+    client.to(nowRoom).emit('peerconnectSignaling', message)
   });
 
   client.on('disconnect', () => {
