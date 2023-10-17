d663e1d3b0166936a43c8aa8ee1c7525c601bea3
a7261675 <a7261675@gmail.com>
2023-10-18 01:03:58 +0800

    add debug log version 1

--- share_screen_server.js
+++ share_screen_server.js
@@ -1,5 +1,5 @@
 // Node.js
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 var bodyParser = require('body-parser');
 var app = require('express')();
 app.use(bodyParser.json());
